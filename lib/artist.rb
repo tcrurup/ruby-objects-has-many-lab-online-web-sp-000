00cc0566ab4b99448ef08746b439a90c43b63b65
Anthony Rurup <tcrurup@gmail.com>
2019-05-01 05:57:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,14 @@
 class Artist 
   
-  attr_accessor :name
-  attr_reader :songs 
+  attr_accessor :name, :songs 
   
   def initialize(name)
     @name = name
     @songs = []
+  end
+  
+  def add_song(song)
+    self.songs << song
   end
   
   
