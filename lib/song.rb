36718327c8905544afd2c92a2680acf3b00bfd40
Anthony Rurup <tcrurup@gmail.com>
2019-05-01 06:29:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,6 +9,14 @@ class Song
     @@all << self
   end
   
+  def artist_name
+    if self.artist.nil?
+      nil
+    else
+      self.artist.name
+    end
+  end
+  
   def self.all 
     @@all
   end
